b10dce70367c4610640ff65f689007a8767a88dc
Unamrr <anna12way@gmail.com>
2026-09-10 21:26:48 +0300

    Create lab1.py

---
+++ lab1.py
@@ -0,0 +1,89 @@
+//1
+n = int(input())
+hours = (n // 60) % 24
+minutes = n % 60
+print(hours, minutes)
+//2
+
+n = int(input("kids: "))
+k = int(input("apples: "))
+print((n-k%n)*(k%n>0))
+//3
+n = int(input("n: "))
+print(n + 2 - n % 2)
+//4
+m = int(input("m: "))
+n = int(input("n: "))
+print((m + n - 1) // n)
+//5
+a = int(input("a = "))
+b = int(input("b = "))
+print((a + b + (a - b) * (1 - 2 * ((a - b) < 0))) // 2)
+
+//1
+n = int(input("N = "))
+if n%2==0:
+    print(n//2)
+else:
+    print(n)
+  //2
+n = int(input("N = "))
+m = int(input("M = "))
+x = int(input("x = "))
+y = int(input("y = "))
+
+if n > m:
+    if x >= n - x:
+        a = n - x
+    else:
+        a = x
+
+    if y >= m - y:
+        b = m - y
+    else:
+        b = y
+else:
+    if x >= m - x:
+        a = m - x
+    else:
+        a = x
+
+    if y >= n - y:
+        b = n - y
+    else:
+        b = y
+
+if a > b:
+    print(b)
+else:
+    print(a)
+
+//1
+# 1
+"""
+a = int(input("A = "))
+b = int(input("B = "))
+for c in range(a+a%2, b+1, 2):
+    print(c)
+"""
+# 2
+"""
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+start = a + (c - a % d) % d
+for i in range(start, b + 1, d):
+    print(i)
+"""
+# 3
+"""
+n = int(input("n = "))
+fact = 1
+s = 0
+for i in range(1, n + 1):
+    fact = fact * i
+    s += fact
+print(sum)
+"""
